3a9e0d468ab7db2c0025ec476ff3c216a93b4a5a
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 10:47:02 +0200

    Update Extrait-EPG.sh

--- EPG/Extrait-EPG.sh
+++ EPG/Extrait-EPG.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-# Dossier contenant les fichiers .xml.gz
-dossier="/home/runner/work/EPG-Rodri/EPG-Rodri/EPG"
-
-dossier_extraits="/home/runner/work/EPG-Rodri/EPG-Rodri/EPG"
-
-# Vérifie si le dossier existe
-if [ ! -d "$dossier" ]; then
-    echo "Le dossier '$dossier' n'existe pas."
-    exit 1
-fi
-
-# Parcours de tous les fichiers .xml.gz dans le dossier
-for fichier in "$dossier"/*.xml.gz; do
-    if [ -e "$fichier" ]; then
-        echo "Traitement de $fichier..."
-        
-        # Décompression et affichage du contenu XML
-        zcat "$fichier" > "$dossier_extraits/$(basename "$fichier" .gz).xml" # Sauvegarde le fichier extrait        
-        echo "Fichier traité : $fichier"
-    else
-        echo "Aucun fichier .xml.gz trouvé dans $dossier."
-        break
-    fi
-done
+gzip /home/runner/work/EPG-Rodri/EPG-Rodri/EPG/epg-iltalehti-fi.xml.gz > /home/runner/work/EPG-Rodri/EPG-Rodri/EPG/epg-iltalehti-fi.xml
