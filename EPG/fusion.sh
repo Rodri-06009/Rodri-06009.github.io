1e6d71dccd285287930b8ea5589cf31cca38f638
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 07:43:44 +0200

    Create fusion.sh

---
+++ EPG/fusion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/runner/work/EPG-Rodri/EPG-Rodri/iptv-org-epg && npm install
+
+cd ../EPG
+
+# Compress EPG xml files
+
+xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
+
+# Remove EPG xml files
+
+rm epg*.xml
+
+exit 0
