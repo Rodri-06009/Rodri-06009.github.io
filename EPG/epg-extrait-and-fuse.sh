51a52244ce1fa5ce316f58f4b39c4ae14f5e7efb
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 14:59:55 +0200

    Update epg-extrait-and-fuse.sh

--- EPG/epg-extrait-and-fuse.sh
+++ EPG/epg-extrait-and-fuse.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-# Vérifiez si des fichiers XML.gz existent avant de décompresser
-if ls epg*.xml.gz 1> /dev/null 2>&1; then
-    # Décompression avec Gzip
-    for file in epg*.xml.gz; do
-        gunzip -k "$file"
-    done
-    
-    # Vérifiez maintenant s'il y a des fichiers XML après décompression
-    if ls epg*.xml 1> /dev/null 2>&1; then
-        # Compression avec XZ
-        xz -k -f -9 epg*.xml && \
-        # Compression avec Gzip
-        gzip -k -f -9 epg*.xml
-    else
-        echo "Aucun fichier XML trouvé après décompression."
-    fi
-else
-    echo "Aucun fichier XML.gz trouvé correspondant à 'epg*.xml.gz'."
-fi
+gzip -k -f -9 ../EPG-Rodri/EPG/epg-iltalehti-fi.xml.gz
 
-# Remove EPG xml files
+gzip -k -f -9 ../EPG-Rodri/EPG/epg-magentatv-de.xml.gz
 
-rm epg*.xml
+gzip -k -f -9 ../EPG-Rodri/EPG/epg-mitv-br.xml.gz
 
-exit 0
+gzip -k -f -9 ../EPG-Rodri/EPG/epg-nos-tv-pt.xml.gz
+
+gzip -k -f -9 ../EPG-Rodri/EPG/epg-tv-ce-soir-fr.xml.gz
