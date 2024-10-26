3e3b7f35ce9b55a69ee1f951306f7492085b4406
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 20:35:13 +0200

    Update Test.sh

--- EPG/Test.sh
+++ EPG/Test.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# URL to test (replace with any file URL)
+# Test URL
 url="https://raw.githubusercontent.com/Rodri-06009/EPG-Rodri/main/EPG/epg-nos-tv-pt.xml"
 
 echo "Testing URL: $url"
-if curl -s --head "$url" | grep "200 OK" > /dev/null; then
+response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+if [ "$response" -eq 200 ]; then
     echo "URL exists. Downloading..."
     curl -s "$url" -o downloaded_file.xml
     echo "File downloaded as downloaded_file.xml"
 else
-    echo "Error: $url not found."
+    echo "Error: $url returned HTTP status $response."
 fi
