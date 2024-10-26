9d4a8cdd49ce451aa90a6a8adf47f3d97cbd0a7d
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 09:22:41 +0200

    Update Extrait-EPG.sh

--- EPG/Extrait-EPG.sh
+++ EPG/Extrait-EPG.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-gzip -d /EPG/epg-tv-ce-soir-fr.xml.gz
+# Chemin vers le répertoire contenant les fichiers .xml.gz
+DIR="/home/Rodri-06009/EPG-Rodri/EPG"
 
-gzip -d /EPG/epg-nos-tv-pt.xml.gz
+# Décompression des fichiers XML
+gzip -d "$DIR/epg-tv-ce-soir-fr.xml.gz"
+gzip -d "$DIR/epg-nos-tv-pt.xml.gz"
+gzip -d "$DIR/epg-mitv-br.xml.gz"
+gzip -d "$DIR/epg-magentatv-de.xml.gz"
+gzip -d "$DIR/epg-iltalehti-fi.xml.gz"
 
-gzip -d /EPG/epg-mitv-br.xml.gz
-
-gzip -d /EPG/epg-magentatv-de.xml.gz
-
-gzip -d /EPG/epg-iltalehti-fi.xml.gz
+# Vérification de l'état de la décompression
+if [ $? -eq 0 ]; then
+    echo "Tous les fichiers ont été décompressés avec succès."
+else
+    echo "Une erreur s'est produite lors de la décompression des fichiers."
+fi
 
 exit 0
