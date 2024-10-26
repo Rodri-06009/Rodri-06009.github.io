958f17ab3865bd67a431d9bdaf970568fb7ccd7f
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 09:27:17 +0200

    Update Extrait-EPG.sh

--- EPG/Extrait-EPG.sh
+++ EPG/Extrait-EPG.sh
@@ -2,11 +2,19 @@
 
 DIR="/home/Rodri-06009/EPG-Rodri/EPG"
 
-# Décompression des fichiers XML avec messages d'erreur
+# Vérifier les fichiers .xml.gz
+echo "Fichiers trouvés :"
+ls "$DIR"/*.xml.gz 2>/dev/null
+
+# Décompression des fichiers XML
 for file in "$DIR"/*.xml.gz; do
-    gzip -d "$file" 2>/dev/null
-    if [ $? -ne 0 ]; then
-        echo "Erreur lors de la décompression de : $file"
+    if [ -e "$file" ]; then
+        gzip -d "$file" 2>/dev/null
+        if [ $? -ne 0 ]; then
+            echo "Erreur lors de la décompression de : $file"
+        fi
+    else
+        echo "Aucun fichier .xml.gz trouvé."
     fi
 done
 
