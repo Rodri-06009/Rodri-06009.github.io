155b63b5e2788ad83b33ab25bd7d556e4e882165
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 09:33:53 +0200

    Update Extrait-EPG.sh

--- EPG/Extrait-EPG.sh
+++ EPG/Extrait-EPG.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-DIR="/EPG-Rodri/EPG"
+gzip -d /home/Rodri-06009/EPG-Rodri/EPG/epg-iltalehti-fi.xml.gz
 
-# Vérifier les fichiers .xml.gz
-echo "Fichiers trouvés :"
-ls "$DIR"/*.xml.gz 2>/dev/null
-
-# Décompression des fichiers XML
-for file in "$DIR"/*.xml.gz; do
-    if [ -e "$file" ]; then
-        gzip -d "$file" 2>/dev/null
-        if [ $? -ne 0 ]; then
-            echo "Erreur lors de la décompression de : $file"
-        fi
-    else
-        echo "Aucun fichier .xml.gz trouvé."
-    fi
-done
-
-echo "Processus terminé."
 exit 0
