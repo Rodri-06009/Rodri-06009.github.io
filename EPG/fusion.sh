a69f1e039479bed6dcec7fa6b679268d6d6fa64a
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 08:43:08 +0200

    Update fusion.sh

--- EPG/fusion.sh
+++ EPG/fusion.sh
@@ -4,4 +4,7 @@
 
 cat ../EPG/epg-iltalehti-fi.xml.gz ../EPG/epg-magentatv-de.xml.gz ../EPG/epg-mitv-br.xml.gz ../EPG/epg-nos-tv-pt.xml.gz ../EPG/epg-tv-ce-soir-fr.xml.gz > epg.xml && gzip -k epg.xml
 
+# Supprime tous les fichiers XML dans le répertoire courant et ses sous-répertoires
+find . -name "*.xml" -type f -delete
+
 exit 0
