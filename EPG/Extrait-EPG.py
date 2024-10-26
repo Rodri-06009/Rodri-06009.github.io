bd5d1ff11784c17ea5bd52cc4eaafb5e6cb9aaee
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 09:52:06 +0200

    Update Extrait-EPG.py

--- EPG/Extrait-EPG.py
+++ EPG/Extrait-EPG.py
@@ -1,29 +1,25 @@
+import lzma
+import xml.etree.ElementTree as ET
 import os
-import gzip
-import shutil
 
-# Répertoire contenant les fichiers .xml.gz
-directory = '/EPG/'
+# Dossier contenant les fichiers .xml.xz
+dossier = os.path.expanduser('../EPG/')
 
-# Liste des fichiers à décompresser
-files_to_decompress = [
-    'epg-tv-ce-soir-fr.xml.gz',
-    'epg-nos-tv-pt.xml.gz',
-    'epg-mitv-br.xml.gz',
-    'epg-magentatv-de.xml.gz',
-    'epg-iltalehti-fi.xml.gz'
-]
-
-for file_name in files_to_decompress:
-    file_path = os.path.join(directory, file_name)
-    
-    if os.path.exists(file_path):
-        # Décompression du fichier
-        with gzip.open(file_path, 'rb') as f_in:
-            with open(file_path[:-3], 'wb') as f_out:  # Enlève '.gz' pour le nom de fichier
-                shutil.copyfileobj(f_in, f_out)
-        print(f'Décompression réussie : {file_path}')
-    else:
-        print(f'Erreur : {file_path} n\'existe pas.')
-
-print("Processus terminé.")
+# Parcours de tous les fichiers dans le dossier
+for nom_fichier in os.listdir(dossier):
+    if nom_fichier.endswith('.xml.xz'):
+        chemin_fichier = os.path.join(dossier, nom_fichier)
+        
+        # Décompression du fichier .xml.xz
+        with lzma.open(chemin_fichier) as fichier:
+            contenu_xml = fichier.read()
+        
+        # Parsing du contenu XML
+        racine = ET.fromstring(contenu_xml)
+        
+        # Exemple : affichage des balises de la racine
+        print(f"Fichier : {nom_fichier}")
+        for enfant in racine:
+            print(enfant.tag, enfant.attrib)
+           
+            exit 0
