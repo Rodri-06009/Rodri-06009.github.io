1363eb5fdb192242bfe4753d4ee807eef297547b
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 12:43:43 +0200

    Update Extrait-EPG.sh

--- EPG/Extrait-EPG.sh
+++ EPG/Extrait-EPG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-zcat EPG-Rodri/EPG/epg-nos-tv-pt.xml.gz > output.xml
-zcat EPG-Rodri/EPG/epg-mitv-br.xml.gz > output.xml
-zcat EPG-Rodri/EPG/epg-magentatv-de.xml.gz > output.xml
-zcat EPG-Rodri/EPG/epg-iltalehti-fi.xml.gz > output.xml
-zcat EPG-Rodri/EPG/epg-tv-ce-soir-fr.xml.gz > output.xml
+zcat home/runner/work/EPG-Rodri/EPG-Rodri/EPG/epg-nos-tv-pt.xml.gz > output.xml
+zcat home/runner/work/EPG-Rodri/EPG-Rodri/EPG/epg-mitv-br.xml.gz > output.xml
+zcat home/runner/work/EPG-Rodri/EPG-Rodri/EPG/epg-magentatv-de.xml.gz > output.xml
+zcat home/runner/work/EPG-Rodri/EPG-Rodri/EPG/epg-iltalehti-fi.xml.gz > output.xml
+zcat home/runner/work/EPG-Rodri/EPG-Rodri/EPG/epg-tv-ce-soir-fr.xml.gz > output.xml
