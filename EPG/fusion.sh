cfcab4a861e01f5d2e318cc8e637a79977655c79
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 08:31:35 +0200

    Update fusion.sh

--- EPG/fusion.sh
+++ EPG/fusion.sh
@@ -2,6 +2,6 @@
 
 # Compress EPG xml files
 
-cat epg-iltalehti-fi.xml.gz epg-magentatv-de.xml.gz epg-mitv-br.xml.gz epg-nos-tv-pt.xml.gz epg-tv-ce-soir-fr.xml.gz > epg.xml && gzip -k epg.xml
+cat ../EPG/epg-iltalehti-fi.xml.gz ../EPG/epg-magentatv-de.xml.gz ../EPG/epg-mitv-br.xml.gz ../EPG/epg-nos-tv-pt.xml.gz ../EPG/epg-tv-ce-soir-fr.xml.gz > epg.xml && gzip -k epg.xml
 
 exit 0
