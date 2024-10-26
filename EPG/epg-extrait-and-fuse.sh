009d7ba9c10987f79c4aba86f825f8681685e2c3
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-26 15:18:01 +0200

    Update epg-extrait-and-fuse.sh

--- EPG/epg-extrait-and-fuse.sh
+++ EPG/epg-extrait-and-fuse.sh
@@ -9,3 +9,9 @@ gunzip -k ../EPG-Rodri/EPG/epg-mitv-br.xml.gz
 gunzip -k ../EPG-Rodri/EPG/epg-nos-tv-pt.xml.gz
 
 gunzip -k ../EPG-Rodri/EPG/epg-tv-ce-soir-fr.xml.gz
+
+cat epg-iltalehti-fi.xml epg-magentatv-de.xml epg-mitv-br.xml epg-nos-tv-pt.xml epg-tv-ce-soir-fr.xml > epg-rodri.xml
+
+gzip -k -f -9 epg-rodri.xml
+
+rm *.xml
