1d6606cd1b6d55a0ec638a4cebb2c5ac0914bf8b
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-27 14:44:35 +0100

    Update epg.sh

--- EPG/epg.sh
+++ EPG/epg.sh
@@ -6,28 +6,6 @@ cd /home/runner/work/EPG-Rodri/EPG-Rodri/iptv-org-epg && npm install
 
 npm run grab -- --channels=../EPG/channels.xml --output=../EPG/epg-rodri.xml --days=7
 
-# TV CE SOIR (France) EPG
-
-npm run grab -- --site=tvcesoir.fr --output=../EPG/epg-tv-ce-soir-fr.xml --days=7
-
-# Nos (Portugal) EPG
-
-npm run grab -- --site=nostv.pt --output=../EPG/epg-nos-pt.xml --days=7
-
-# Iltalehti (Finland) EPG
-
-npm run grab -- --site=iltalehti.fi --output=../EPG/epg-iltalehti-fi.xml --days=7
-
-# Magentatv (Germany) EPG
-
-npm run grab -- --site=web.magentatv.de --output=../EPG/epg-magentatv-de.xml --days=7
-
-# mi.tv (Brasil) EPG
-
-npm run grab -- --channels=/EPG/mi.tv_br.channels.xml --output=../EPG/epg-mi-tv-br.xml --days=7
-
-cd ../EPG
-
 # Compress EPG xml files
 
 xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
