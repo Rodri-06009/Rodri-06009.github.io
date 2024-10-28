d90fe5cbf5e0961bb40a704d08ec1b498cc8d5af
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-28 20:21:53 +0100

    Update epg.sh

--- EPG/epg.sh
+++ EPG/epg.sh
@@ -6,12 +6,4 @@ cd /home/runner/work/EPG-Rodri/EPG-Rodri/iptv-org-epg && npm install
 
 npm run grab -- --channels=../EPG/channels.xml --output=../EPG/epg-rodri.xml --days=7
 
-# Compress EPG xml files
-
-xz -k -f -9 EPG/epg-rodri.xml && gzip -k -f -9 EPG/epg-rodri.xml
-
-# Remove EPG xml files
-
-rm EPG/epg-rodri.xml
-
 exit 0
