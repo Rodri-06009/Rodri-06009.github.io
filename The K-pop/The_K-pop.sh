609baea4748f589be82d0ac50cf11e77889413d9
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-31 12:42:57 +0100

    Update The_K-pop.sh

--- The K-pop/The_K-pop.sh
+++ The K-pop/The_K-pop.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 The K-pop.py > ../The K-pop/The K-pop.m3u8
+python3 The_K-pop.py > ../The K-pop/The_K-pop.m3u8
 
 echo m3u grabbed
