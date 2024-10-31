1c9a49ea3cc3f251f4561f67a687d333e7d6198f
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-31 13:35:06 +0100

    Update The_K-pop.sh

--- The_K-pop/The_K-pop.sh
+++ The_K-pop/The_K-pop.sh
@@ -9,7 +9,7 @@ cd "$SCRIPT_DIR" || { echo "Échec de la navigation vers $SCRIPT_DIR"; exit 1; }
 
 # Exécuter le script Bash et rediriger la sortie vers le fichier .m3u8
 OUTPUT_FILE="../The_K-pop/The_K-pop.m3u8"
-bash The_K-pop.sh > "$OUTPUT_FILE"
+bash The_K-pop-2.sh > "$OUTPUT_FILE"
 
 # Vérifier si le fichier a été créé avec succès
 if [ -f "$OUTPUT_FILE" ]; then
