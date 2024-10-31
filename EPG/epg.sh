a973caf61dbe1127db7ea33abbeaee826242796b
Rodri-06009 <gamingrodrigo2009@hotmail.com>
2024-10-31 12:11:02 +0100

    Delete EPG directory

--- EPG/epg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/runner/work/Rodri-06009.github.io/Rodri-06009.github.io/iptv-org-epg && npm install
-
-# EPG
-
-npm run grab -- --channels=../EPG/channels.xml --output=../EPG/epg-rodri.xml --days=7
-
-exit 0
